e337ba1615c6918da01b21f6049093a9346e0039
Artem Tkachenko <tomsoir@gmail.com>
2017-02-27 10:17:11 -0800

    Enforce two new experiments

--- src/app/reducers/accounts.js
+++ src/app/reducers/accounts.js
@@ -13,6 +13,28 @@ export default function(state=DEFAULT, action={}) {
 
     case accountActions.RECEIVED_ACCOUNT: {
       const { accounts } = action.apiResponse;
+
+      // Enforce new experiments
+      // @TODO: remove this after tests
+      Object.keys(accounts).forEach(accountName => {
+        const account = accounts[accountName];
+        accounts[accountName] = account.set({
+          features: {
+            ...(account.features || {}),
+            mweb_xpromo_interstitial_comments_ios: {
+              variant: 'treatment',
+              owner: 'channels',
+              experiment_id: 136,
+            },
+            mweb_xpromo_require_login_ios: {
+              variant: 'login_required',
+              owner: 'channels',
+              experiment_id: 133,
+            },
+          },
+        });
+      });
+
       return mergeAPIModels(state, accounts);
     }
 
